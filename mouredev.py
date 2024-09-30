90fa46aad4b30661fd57c45923bdda27b435b236
Lucy Hernandez Avendaño <tatianahernandezavendano@Lucy-MacBook-Air.local>
2024-09-30 21:25:10 +0200

    Ejercicios c

--- mouredev.py
+++ mouredev.py
@@ -16,9 +16,9 @@ while aniversary_year <= years_of_creation + 100:
  first_saturday = 5 - september.weekday() if september.weekday() <= 5 else 12 - \
      september.weekday()
      
-     third_saturday = september + timedelta(days=first_saturday +14)
+third_saturday = september + timedelta(days=first_saturday +14)
      
-     batman_day_aniversary_dates.append(
+batman_day_aniversary_dates.append(
          (
             aniversary_year,
             aniversary_year-years_of_creation,
@@ -26,27 +26,25 @@ while aniversary_year <= years_of_creation + 100:
              )
         )
      
-     aniversary_year += 1
+aniversary_year += 1
      
-     for year,aniversary, batman_day, batman_day_aniversary_dates:
-          
-     print(f"batman day {year} ({aniversary} anivesario): {batman_day}")
+for year,aniversary, batman_day, batman_day_aniversary_dates:
+print(f"batman day {year} ({aniversary} anivesario): {batman_day}")
      
      
      #reto 2
 def sum_subgrid_alerts(sensors, center_x , center_y) -> int:
     
     total = 0
-    
     for x in range(center_x-1 , center_x + 2) 
         
      
       
 def  batcave_segurity_sistem(sensors):
-      for x in range(1, 19):
+      for x in range(0, 19):
        for y  in range(1, 19):
            alert_level = sum_subgrid_alerts(sensors,x,y)
-          # if alert_level > max_alert_level:
+           #if alert_level > max_alert_level:
             # max_alert_level
                 
  
